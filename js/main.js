d37c79330fe6812fc6cfbe8c9c298ee39be76a85
zile028 <zile028@gmail.com>
2021-05-09 15:58:55 +0200

    typed, slider, popup news and  responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+/*--- typed ---*/
+var typed = new Typed(".element", {
+  strings: [
+    "radimo custom WP",
+    "postavljamo sajt na server.",
+    "radimo front-end sajta.",
+  ],
+  typeSpeed: 30,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|",
+  backDelay: 700,
+});
+
 /*--- slider ---*/
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
-  nav: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  smartSpeed: 700,
   responsive: {
     0: {
       items: 1,
@@ -11,7 +28,17 @@ $(".owl-carousel").owlCarousel({
       items: 3,
     },
     1000: {
-      items: 5,
+      items: 3,
     },
   },
 });
+
+/*--- news ---*/
+let news = document.querySelector(".news");
+const showNews = () => {
+  news.classList.toggle("showNews");
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 2000);
+};
+const interval = setInterval(showNews, 2000);
